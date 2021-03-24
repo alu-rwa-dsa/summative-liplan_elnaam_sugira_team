ddc7b7675856e48ce4e7fcf486b27890a3fb8edd
Elnaam10 <e.umutoni@alustudent.com>
2021-03-24 04:17:47 -0700

    main

---
+++ main.py
@@ -0,0 +1,33 @@
+import PhoneBook
+import csv
+import time
+
+#here we will add our contanct information as linkedlist
+def add_all_contact_info(linkedlist):
+    with open("C:\Users\unknown\summative-liplan_elnaam_sugira_team") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                # skip headers
+                line_count += 1
+            else:
+                # add name and surname, cols 1, 2
+                linkedlist.add_contact(row[0], row[1])
+                # add address, col 3
+                linkedlist.find_contacts(row[0], row[1]).address.add_address(row[3])
+                # add phones, cols 8, 9
+                linkedlist.find_contacts(row[0], row[1]).address.add_phone(row[8])
+                linkedlist.find_contacts(row[0], row[1]).address.add_phone(row[9], "Home")
+                # add email, col 10
+                linkedlist.find_contacts(row[0], row[1]).address.add_email(row[10])
+                # go to next line
+                line_count += 1
+
+
+start = time.time()
+new_linkedList = PhoneBook()
+add_all_contact_info(new_linkedList)
+end = time.time()
+print("Execution time: {}".format(end-start))
+
